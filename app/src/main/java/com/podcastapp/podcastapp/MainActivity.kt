2c1b1adde4ce19df53b33a9086e92bfce6e6c061
vanderouana <vander@keyo.co>
2025-01-22 11:40:21 -0800

    Home page added

--- app/src/main/java/com/podcastapp/podcastapp/MainActivity.kt
+++ app/src/main/java/com/podcastapp/podcastapp/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,14 +20,30 @@ class MainActivity : ComponentActivity() {
         setContent {
             PodcastAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    PodcastDetails(
+                    PodcastsHome(
                         modifier = Modifier.padding(innerPadding),
-                        Podcast(
-                            1,
-                            "The Indicator from Planet Money",
-                            "NPR",
-                            "https://cdn-images-3.listennotes.com/podcasts/the-diary-of-a-ceo-with-steven-bartlett-doac-wSgVQrueZOJ-Gflmgre3zuU.800x800.jpg",
-                            "A few years ago I was a broke, university dropout, at 18 I built an industry leading social media marketing company, and at 27 I resigned as CEO. At 28 I co-founded Flight Story – a marketing and communications company, and thirdweb - a software platform, making it easy to build web3 applications. I then launched private equity fund, Flight Fund, to accelerate the next generation of European unicorns. During this time I decided to launch 'The Diary Of A CEO' podcast with the simple mission of providing an unfiltered journey into the remarkable stories and untold dimensions of the world’s most influential people, experts and thinkers. Thank you for listening. My New Book: https://g2ul0.app.link/DOAC IG: https://www.instagram.com/steven LI: https://www.linkedin.com/in/stevenbartlett-123"
+                        listOf(
+                            Podcast(
+                                1,
+                                "The Indicator from Planet Money",
+                                "NPR",
+                                "https://cdn-images-3.listennotes.com/podcasts/the-diary-of-a-ceo-with-steven-bartlett-doac-wSgVQrueZOJ-Gflmgre3zuU.800x800.jpg",
+                                "A few years ago I was a broke, university dropout, at 18 I built an industry leading social media marketing company, and at 27 I resigned as CEO. At 28 I co-founded Flight Story – a marketing and communications company, and thirdweb - a software platform, making it easy to build web3 applications. I then launched private equity fund, Flight Fund, to accelerate the next generation of European unicorns. During this time I decided to launch 'The Diary Of A CEO' podcast with the simple mission of providing an unfiltered journey into the remarkable stories and untold dimensions of the world’s most influential people, experts and thinkers. Thank you for listening. My New Book: https://g2ul0.app.link/DOAC IG: https://www.instagram.com/steven LI: https://www.linkedin.com/in/stevenbartlett-123"
+                            ),
+                            Podcast(
+                                1,
+                                "The Indicator from Planet Money",
+                                "NPR",
+                                "https://cdn-images-3.listennotes.com/podcasts/the-diary-of-a-ceo-with-steven-bartlett-doac-wSgVQrueZOJ-Gflmgre3zuU.800x800.jpg",
+                                "A few years ago I was a broke, university dropout, at 18 I built an industry leading social media marketing company, and at 27 I resigned as CEO. At 28 I co-founded Flight Story – a marketing and communications company, and thirdweb - a software platform, making it easy to build web3 applications. I then launched private equity fund, Flight Fund, to accelerate the next generation of European unicorns. During this time I decided to launch 'The Diary Of A CEO' podcast with the simple mission of providing an unfiltered journey into the remarkable stories and untold dimensions of the world’s most influential people, experts and thinkers. Thank you for listening. My New Book: https://g2ul0.app.link/DOAC IG: https://www.instagram.com/steven LI: https://www.linkedin.com/in/stevenbartlett-123"
+                            ),
+                            Podcast(
+                                1,
+                                "The Indicator from Planet Money",
+                                "NPR",
+                                "https://cdn-images-3.listennotes.com/podcasts/the-diary-of-a-ceo-with-steven-bartlett-doac-wSgVQrueZOJ-Gflmgre3zuU.800x800.jpg",
+                                "A few years ago I was a broke, university dropout, at 18 I built an industry leading social media marketing company, and at 27 I resigned as CEO. At 28 I co-founded Flight Story – a marketing and communications company, and thirdweb - a software platform, making it easy to build web3 applications. I then launched private equity fund, Flight Fund, to accelerate the next generation of European unicorns. During this time I decided to launch 'The Diary Of A CEO' podcast with the simple mission of providing an unfiltered journey into the remarkable stories and untold dimensions of the world’s most influential people, experts and thinkers. Thank you for listening. My New Book: https://g2ul0.app.link/DOAC IG: https://www.instagram.com/steven LI: https://www.linkedin.com/in/stevenbartlett-123"
+                            )
                         )
                     )
                 }
@@ -37,18 +52,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PodcastAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/podcastapp/podcastapp/PodcastsHome.kt
+++ app/src/main/java/com/podcastapp/podcastapp/PodcastsHome.kt
@@ -6,31 +6,71 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.podcastapp.podcastapp.mods.Podcast
 
 
 @Composable
-fun PodcastsHome() {
+fun PodcastsHome(modifier: Modifier = Modifier, podcasts: List<Podcast>) {
+    Column(modifier = modifier.fillMaxSize().padding(top =15.dp)) {
+        Text(
+            text = stringResource(R.string.podcasts),
+            style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 25.sp)
+        )
+        LazyColumn {
+            items(podcasts) { podcast ->
+                PodcastItem(podcast) {
 
+                }
+            }
+        }
+    }
 }
 
 @Composable
-fun PodcastItem(podcast: Podcast) {
+fun PodcastItem(podcast: Podcast, onPodcastClicked: () -> Unit) {
+    Column(modifier = Modifier.clickable { onPodcastClicked() }) {
+        Row {
+            AsyncImage(
+                model = podcast.iconUrl,
+                modifier = Modifier.size(15.dp),
+                contentDescription = null,
+            )
+
+            Column(modifier = Modifier.padding(top = 25.dp)) {
+                Text(
+                    text = podcast.name,
+                    style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 15.sp)
+                )
+                Text(text = podcast.hostName, color = Color.Gray)
+                Text(text = stringResource(R.string.favourited), color = Color.Red)
+            }
+
+        }
+        Divider(modifier = Modifier.padding(horizontal = 15.dp))
+    }
 
 }
-
-
 
 
 @Composable
